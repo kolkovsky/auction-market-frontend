97b1d2e1eebf66ca59d2c415957d371405d5b495
Stas Kolkovsky <stas.kolkovsky@gmail.com>
2022-06-05 17:06:49 +0500

    Init commit

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import { Container, Typography, Box } from "@mui/material";
+import React from "react";
+
+
+const LoginPage = () => {
+    return (
+        <Container>
+            <Box>
+                <Typography component="h4">
+                    Login in website:
+                </Typography>   
+            </Box>
+        </Container>
+    )
+};
+
+export default LoginPage;
